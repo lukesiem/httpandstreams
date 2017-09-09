7eadb7b2148abad313d54d6fb60b30fab6787ef5
Luke Siemens <lukesiemens@gmail.com>
2017-09-09 23:07:20 +0000

    testing a request

--- https_example.js
+++ https_example.js
@@ -1,3 +1,19 @@
 var https = require('https');
 
 console.log('I did it!');
+
+var options = {
+	host: 'www.example.org',
+	path: '/'
+};
+
+var callback = function() {
+	console.log('In response hander callback!');
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options,callback).end();
+
+console.log("i've made the request");
+
